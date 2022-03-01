6e0dae8a33920a2e83e19983b69aae95e8addf95
Francesco Peleggi <francesco.peleggi@keypartner.com>
2022-03-01 21:01:27 +0100

    openclose-service: added configuration for replicaFactor and partitionNumber

--- spring-boot/microservices/openclose-service/src/main/java/com/tech/challenge/openclose/service/config/TopicConfiguration.java
+++ spring-boot/microservices/openclose-service/src/main/java/com/tech/challenge/openclose/service/config/TopicConfiguration.java
@@ -1,7 +1,9 @@
 package com.tech.challenge.openclose.service.config;
 
 import lombok.Data;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
@@ -10,4 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class TopicConfiguration {
 
     private String restaurantsTopic;
+    private Integer partitionNumber;
+    private Short replicaFactor;
+
+    @Bean
+    public NewTopic adviceTopic(){
+        return new NewTopic(this.getRestaurantsTopic(),this.getPartitionNumber(),this.getReplicaFactor());
+    }
+
 }
