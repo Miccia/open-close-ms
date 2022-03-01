028a35d595593cea8aead93a488ef1aeae7919a9
Francesco Peleggi <francesco.peleggi@keypartner.com>
2022-03-01 18:54:37 +0100

    consumer config

---
+++ spring-boot/microservices/frontend-service/src/main/java/com/tech/challenge/frontend/service/consumers/OpenCloseKafkaListener.java
@@ -0,0 +1,30 @@
+package com.tech.challenge.frontend.service.consumers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tech.challenge.frontend.service.model.dto.RestaurantDTO;
+import com.tech.challenge.frontend.service.service.RestaurantEntityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenCloseKafkaListener {
+
+    private Logger log = LoggerFactory.getLogger(OpenCloseKafkaListener.class);
+
+    @Autowired
+    ObjectMapper objMapper;
+
+    @Autowired
+    RestaurantEntityService restaurantService;
+
+    @KafkaListener(topics="${spring.kafka.consumerTopic}")
+    void listen(String data){
+        log.info("incoming data:{}",data);
+       
+    }
+
+
+}
