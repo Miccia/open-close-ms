6a09404a5b8eef0b50142d81f279eafd5153b5b4
Francesco Peleggi <francesco.peleggi@keypartner.com>
2022-03-01 19:00:53 +0100

    implementing be rest service logic

--- spring-boot/microservices/openclose-service/src/main/java/com/tech/challenge/openclose/service/rest/controller/OpenCloseServiceRestController.java
+++ spring-boot/microservices/openclose-service/src/main/java/com/tech/challenge/openclose/service/rest/controller/OpenCloseServiceRestController.java
@@ -1,7 +1,14 @@
 package com.tech.challenge.openclose.service.rest.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.tech.challenge.openclose.service.config.TopicConfiguration;
+import com.tech.challenge.openclose.service.model.dto.RestaurantDTO;
+import com.tech.challenge.openclose.service.service.MessagePublisher;
+import com.tech.challenge.openclose.service.service.RestaurantEntityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,6 +17,25 @@ public class OpenCloseServiceRestController {
 
     private static final Logger log = LoggerFactory.getLogger(OpenCloseServiceRestController.class);
 
+    @Autowired
+    MessagePublisher kProducer;
+    @Autowired
+    RestaurantEntityService entityService;
+    @Autowired
+    TopicConfiguration topicConfig;
+    
+    @PostMapping(value="update/{id}")
+    public ResponseEntity<String> updateRestaurantStatus(@PathVariable("id") String id,
+                                                         @RequestParam Boolean isOpen) throws JsonProcessingException {
+
+        entityService.updateRestaurantStatus(id,isOpen);
+        RestaurantDTO restaurant = new RestaurantDTO();
+        restaurant.setId(id);
+        restaurant.setIsOpen(isOpen);
+        kProducer.publishObject(topicConfig.getRestaurantsTopic(),restaurant);
+
+        return ResponseEntity.ok("");
+    }
 
 
 }
