ede9c7f06ce3cdb9fac9494f4dbf98a0ec9ff8d2
Francesco Peleggi <francesco.peleggi@keypartner.com>
2022-03-02 02:27:56 +0100

    simple react implementation

---
+++ spring-boot/microservices/frontend-service/src/main/js/app.js
@@ -0,0 +1,61 @@
+'use strict';
+import 'regenerator-runtime/runtime';
+const React = require('react');
+const ReactDOM = require('react-dom');
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {restaurants: []};
+	}
+
+	async componentDidMount() {
+    const response = await fetch("/restaurants/list/OPEN");
+    const jsonData = await response.json();
+    this.setState({restaurants:jsonData})
+	}
+
+	render() {
+		return (
+			<RestaurantList restaurants={this.state.restaurants}/>
+		)
+	}
+}
+
+class RestaurantList extends React.Component{
+	render() {
+	console.log(this.props.restaurants);
+		const restaurants = Array.from(this.props.restaurants).map(restaurant =>
+			<Restaurant key={restaurant.id} restaurant={restaurant}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>Name</th>
+						<th>Description</th>
+						<th>Address</th>
+					</tr>
+					{restaurants}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class Restaurant extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.restaurant.name}</td>
+				<td>{this.props.restaurant.description}</td>
+				<td>{this.props.restaurant.address}</td>
+			</tr>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
