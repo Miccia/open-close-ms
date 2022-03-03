38d5bff71db431b6ffed95cb0d68619dd5cf0049
Francesco Peleggi <francesco.peleggi@keypartner.com>
2022-03-03 01:36:16 +0100

    fixed kafka producer config + set kafka replica factor to 1

--- spring-boot/microservices/openclose-service/src/main/java/com/tech/challenge/openclose/service/config/KafkaProducerConfig.java
+++ spring-boot/microservices/openclose-service/src/main/java/com/tech/challenge/openclose/service/config/KafkaProducerConfig.java
@@ -1,5 +1,7 @@
 package com.tech.challenge.openclose.service.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +9,13 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -18,21 +23,24 @@ public class KafkaProducerConfig {
     @Autowired
     KafkaConfiguration kafkaConfiguration;
 
-
+    @Bean
+    KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,Arrays.asList(kafkaConfiguration.getBrokers().split(",")));
+        return new KafkaAdmin(configs);
+    }
     @Bean
     public ProducerFactory<String, String> producerFactory() {
-
-        KafkaProperties properties = new KafkaProperties();
-        properties.getProducer().setBootstrapServers(Arrays.asList(kafkaConfiguration.getBrokers().split(",")));
-        properties.getProducer().setKeySerializer(StringSerializer.class);
-        properties.getProducer().setValueSerializer(StringSerializer.class);
-        properties.getProducer().setClientId(kafkaConfiguration.getProducerId());
-        return new DefaultKafkaProducerFactory<>(properties.buildProducerProperties());
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfiguration.getBrokers());
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.CLIENT_ID_CONFIG, kafkaConfiguration.getProducerId());
+        return new DefaultKafkaProducerFactory<>(configProps);
     }
-
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        return new KafkaTemplate<>(this.producerFactory());
     }
 
     @Bean
