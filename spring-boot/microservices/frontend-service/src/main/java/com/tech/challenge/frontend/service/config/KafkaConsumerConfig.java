1163f188b427bcd79495ec81b25f4d66e175776d
Francesco Peleggi <francesco.peleggi@keypartner.com>
2022-03-03 01:10:42 +0100

    fixed kafka consumer configuration and docker-compose w fe-service

--- spring-boot/microservices/frontend-service/src/main/java/com/tech/challenge/frontend/service/config/KafkaConsumerConfig.java
+++ spring-boot/microservices/frontend-service/src/main/java/com/tech/challenge/frontend/service/config/KafkaConsumerConfig.java
@@ -2,11 +2,13 @@ package com.tech.challenge.frontend.service.config;
 
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -20,16 +22,23 @@ public class KafkaConsumerConfig {
     @Autowired
     private KafkaConfiguration kafkaConfiguration;
 
+
     @Bean
-    public ConsumerFactory<String, String> consumerFactory(KafkaProperties properties) {
-
-        properties.getConsumer().setBootstrapServers(Arrays.asList(kafkaConfiguration.getBrokers().split(",")));
-        properties.getConsumer().setGroupId(kafkaConfiguration.getGroupId());
-        properties.getConsumer().setClientId(kafkaConfiguration.getConsumerId());
-        properties.getConsumer().setIsolationLevel(KafkaProperties.IsolationLevel.READ_COMMITTED);
-        properties.getConsumer().setValueDeserializer(StringDeserializer.class);
-        properties.getConsumer().setKeyDeserializer(StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties());
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfiguration.getBrokers());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfiguration.getGroupId());
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, kafkaConfiguration.getConsumerId());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, kafkaConfiguration.getIsolationLevel());
+        return new DefaultKafkaConsumerFactory<>(props);
     }
-
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
